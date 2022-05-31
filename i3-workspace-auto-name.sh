d2f079c10f50e5e325f4a45f72fb04bc66e79852
Archer <z@zt.vc>
2022-05-31 20:05:44 +0800

    old version

--- i3-workspace-auto-name.sh
+++ i3-workspace-auto-name.sh
@@ -4,9 +4,6 @@
 # github: https://github.com/rains31
 DISPLAY=${DISPLAY:-":0"}
 MAX_NAME_LENGTH=20
-# force update interval for inactive workspaces
-UPDATE_INTERVAL=60
-CACHE_FILE=/tmp/.i3-inactive-workspaces
 DEBUG=false
 
 watch_wm_name() {
@@ -21,7 +18,7 @@ rename_workspace() {
   local num=$1
   local oldname=$2
   local newname=$3
-  newname=$(echo $newname | tr -d '<>"\\')
+  newname=$(echo $newname | tr -d '<>"\\') # remove special chars
   newname=$([ ${#newname} -eq 0 ] && echo $num || echo $num:${newname:0:$MAX_NAME_LENGTH})
   [ "$oldname" = "$newname" ] || {
     msg='rename workspace "'$oldname'" to "'$newname'"'
@@ -30,30 +27,23 @@ rename_workspace() {
   }
 }
 
-workspaces_not_changed() {
-  oldsum=$(cat $CACHE_FILE | md5sum)
-  newsum=$(i3-msg -t get_tree | jq '.nodes[].nodes[].nodes[] | select(.type=="workspace" and .focused!=true)' | tee $CACHE_FILE | md5sum)
-  [ "$oldsum" = "$newsum" ]
-  return $?
-}
-
 update_workspaces() {
   local wname=$1
-  # do nothing if workspace tree not changed
-  [ -z "$wname" ] && workspaces_not_changed && return
   i3-msg -t get_workspaces | jq '.[]' | jq '.focused,.num,.name' | xargs -n3 | while read focused num oldname; do {
+    # echo focused=$focused num=$num name=$oldname
     if [ $focused = 'true' ]; then
       # in the current workspace, rename workspace to the active window name
-      [ -z "$wname" ] || rename_workspace $num "$oldname" "$wname"
+      rename_workspace $num "$oldname" "$wname"
     else
-      # new workspace or force update
-      [ ${#oldname} -lt 3 -o -z "$wname" ] && {
-        ws=$(cat $CACHE_FILE | jq 'select(.num=='$num')')
-        echo $ws | jq '.focus[]' | while read focus; do {
+      # new workspace
+      [ ${#oldname} -eq 1 ] && {
+        ws=$(i3-msg -t get_tree | jq '.nodes[].nodes[].nodes[] | select(.type=="workspace" and .num=='$num')')
+        focus=$(echo $ws | jq '.focus[]' | head -n1)
+        # in a new workspace with a forcus window, use the focus window name
+        [ -z $focus ] || {
           focusname=$(echo $ws | jq -r '.nodes[] | select(.id=='$focus').name')
-          # use the focus window name unless it's not null
-          [ "$focusname" = 'null' ] || rename_workspace $num "$oldname" "$focusname"
-        }; done
+          rename_workspace $num "$oldname" "$focusname"
+        }
       }
     fi
   }; done
@@ -61,9 +51,4 @@ update_workspaces() {
 
 watch_wm_name | while read wname; do {
   update_workspaces "${wname}"
-}; done &
-
-while true; do
-  update_workspaces
-  sleep $UPDATE_INTERVAL
-done
+}; done
